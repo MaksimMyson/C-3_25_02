bbc9213c7d24669abd0e414ad80fb892b74aec53
MaksimMyson <maksimmyson33@gmail.com>
2024-03-03 15:42:54 +0200

    exe2

--- C#3_25_02/Program.cs
+++ C#3_25_02/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace C_3_25_02
+﻿using Core;
+namespace C_3_25_02
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Введіть число для перевірки:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            bool isFibonacci = NumberHelper.IsFibonacci(number);
+            bool isPrime = NumberHelper.IsPrime(number);
+
+            Console.WriteLine($"Число {number} є числом Фібоначчі: {isFibonacci}");
+            Console.WriteLine($"Число {number} є простим числом: {isPrime}");
         }
     }
 }
--- Core/Model.cs
+++ Core/Model.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace Core
 {
-    internal class Model
+    public class NumberHelper
     {
+        public static bool IsFibonacci(int number)
+        {
+            return IsPerfectSquare(5 * number * number + 4) || IsPerfectSquare(5 * number * number - 4);
+        }
+
+        private static bool IsPerfectSquare(int x)
+        {
+            int sqrt = (int)Math.Sqrt(x);
+            return sqrt * sqrt == x;
+        }
+
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1)
+                return false;
+            if (number <= 3)
+                return true;
+            if (number % 2 == 0 || number % 3 == 0)
+                return false;
+            int i = 5;
+            while (i * i <= number)
+            {
+                if (number % i == 0 || number % (i + 2) == 0)
+                    return false;
+                i += 6;
+            }
+            return true;
+        }
     }
 }
