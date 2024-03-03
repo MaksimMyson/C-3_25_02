56ca60d1c1f9f481ab72f1c3e472004dd301d078
MaksimMyson <maksimmyson33@gmail.com>
2024-03-03 17:02:14 +0200

    exe7

--- C#3_25_02/Program.cs
+++ C#3_25_02/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace C_3_25_02
+﻿using Core;
+namespace C_3_25_02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           Console.OutputEncoding = System.Text.Encoding.Unicode;
+            
+            Matrix matrix = new Matrix(3, 3);
+
+            
+            matrix.InputMatrix();
+
+            
+            matrix.DisplayMatrix();
+
+            
+            Console.WriteLine($"Максимальний елемент матриці: {matrix.MaxElement()}");
+            Console.WriteLine($"Мінімальний елемент матриці: {matrix.MinElement()}");
         }
     }
 }
--- Core/Model.cs
+++ Core/Model.cs
@@ -6,7 +6,88 @@ using System.Threading.Tasks;
 
 namespace Core
 {
-    internal class Model
+    public class Matrix
     {
+        private int[,] data;
+        private int rows;
+        private int cols;
+
+      
+        public Matrix(int rows, int cols)
+        {
+            this.rows = rows;
+            this.cols = cols;
+            data = new int[rows, cols];
+        }
+
+        
+        public Matrix(int[,] data)
+        {
+            this.rows = data.GetLength(0);
+            this.cols = data.GetLength(1);
+            this.data = data;
+        }
+
+        
+        public void InputMatrix()
+        {
+            Console.WriteLine("Введіть елементи матриці:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"Елемент [{i + 1},{j + 1}]: ");
+                    data[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+
+        
+        public void DisplayMatrix()
+        {
+            Console.WriteLine("Матриця:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(data[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+       
+        public int MaxElement()
+        {
+            int max = data[0, 0];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (data[i, j] > max)
+                    {
+                        max = data[i, j];
+                    }
+                }
+            }
+            return max;
+        }
+
+     
+        public int MinElement()
+        {
+            int min = data[0, 0];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (data[i, j] < min)
+                    {
+                        min = data[i, j];
+                    }
+                }
+            }
+            return min;
+        }
     }
 }
