599649ba726bcffc68fc2027754239d939f7c773
MaksimMyson <maksimmyson33@gmail.com>
2024-03-03 22:02:41 +0200

    exe6

--- C#3_25_02/Program.cs
+++ C#3_25_02/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace C_3_25_02
+﻿using Core;
+
+namespace C_3_25_02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Store store = new Store();
+
+            Console.WriteLine("Введіть дані про магазин:");
+            store.InputData();
+
+            Console.WriteLine("\nІнформація про магазин:");
+            store.DisplayData();
         }
     }
 }
--- Core/Model.cs
+++ Core/Model.cs
@@ -6,7 +6,89 @@ using System.Threading.Tasks;
 
 namespace Core
 {
-    internal class Model
+    public class Store
     {
+        private string name;
+        private string address;
+        private string profileDescription;
+        private string contactPhone;
+        private string email;
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetAddress(string address)
+        {
+            this.address = address;
+        }
+
+        public string GetAddress()
+        {
+            return address;
+        }
+
+        public void SetProfileDescription(string profileDescription)
+        {
+            this.profileDescription = profileDescription;
+        }
+
+        public string GetProfileDescription()
+        {
+            return profileDescription;
+        }
+
+        public void SetContactPhone(string contactPhone)
+        {
+            this.contactPhone = contactPhone;
+        }
+
+        public string GetContactPhone()
+        {
+            return contactPhone;
+        }
+
+        public void SetEmail(string email)
+        {
+            this.email = email;
+        }
+
+        public string GetEmail()
+        {
+            return email;
+        }
+
+        public void InputData()
+        {
+            Console.WriteLine("Введіть назву магазину:");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Введіть адресу магазину:");
+            address = Console.ReadLine();
+
+            Console.WriteLine("Введіть опис профілю магазину:");
+            profileDescription = Console.ReadLine();
+
+            Console.WriteLine("Введіть контактний телефон магазину:");
+            contactPhone = Console.ReadLine();
+
+            Console.WriteLine("Введіть email магазину:");
+            email = Console.ReadLine();
+        }
+
+        public void DisplayData()
+        {
+            Console.WriteLine($"Назва магазину: {name}");
+            Console.WriteLine($"Адреса: {address}");
+            Console.WriteLine($"Опис профілю магазину: {profileDescription}");
+            Console.WriteLine($"Контактний телефон: {contactPhone}");
+            Console.WriteLine($"Email: {email}");
+        }
     }
 }
