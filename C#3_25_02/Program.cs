7edaaa73d57eae78af7ce693a35e544fb2bd7b3c
MaksimMyson <maksimmyson33@gmail.com>
2024-03-03 17:16:46 +0200

    exe1

--- C#3_25_02/Program.cs
+++ C#3_25_02/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace C_3_25_02
+﻿using Core;
+
+namespace C_3_25_02
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.Unicode; 
+
+            Console.WriteLine("Введіть довжину сторони квадрата:");
+            int sideLength = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введіть символ для відображення квадрата:");
+            char symbol = Console.ReadLine()[0];
+
+            SquareDrawer.DrawSquare(sideLength, symbol);
         }
     }
 }
--- Core/Model.cs
+++ Core/Model.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Core
 {
-    internal class Model
+    public class SquareDrawer
     {
+        public static void DrawSquare(int sideLength, char symbol)
+        {
+            if (sideLength <= 0)
+            {
+                Console.WriteLine("Сторона квадрата повинна бути більше нуля.");
+                return;
+            }
+
+            for (int i = 0; i < sideLength; i++)
+            {
+                for (int j = 0; j < sideLength; j++)
+                {
+                    Console.Write(symbol + " ");
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
