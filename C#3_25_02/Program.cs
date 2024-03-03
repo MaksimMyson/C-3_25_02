3db966d52e7fbf6ce029f9f54ce2c1896d933009
MaksimMyson <maksimmyson33@gmail.com>
2024-03-03 21:44:25 +0200

    exe4

--- C#3_25_02/Program.cs
+++ C#3_25_02/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace C_3_25_02
+﻿using Core;
+
+namespace C_3_25_02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Website website = new Website();
+
+            Console.WriteLine("Введіть дані про веб-сайт:");
+            website.InputData();
+
+            Console.WriteLine("\nІнформація про веб-сайт:");
+            website.DisplayData();
         }
     }
 }
--- Core/Model.cs
+++ Core/Model.cs
@@ -6,7 +6,74 @@ using System.Threading.Tasks;
 
 namespace Core
 {
-    internal class Model
+    public class Website
     {
+        private string name;
+        private string url;
+        private string description;
+        private string ipAddress;
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetUrl(string url)
+        {
+            this.url = url;
+        }
+
+        public string GetUrl()
+        {
+            return url;
+        }
+
+        public void SetDescription(string description)
+        {
+            this.description = description;
+        }
+
+        public string GetDescription()
+        {
+            return description;
+        }
+
+        public void SetIpAddress(string ipAddress)
+        {
+            this.ipAddress = ipAddress;
+        }
+
+        public string GetIpAddress()
+        {
+            return ipAddress;
+        }
+
+        public void InputData()
+        {
+            Console.WriteLine("Введіть назву сайту:");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Введіть URL сайту:");
+            url = Console.ReadLine();
+
+            Console.WriteLine("Введіть опис сайту:");
+            description = Console.ReadLine();
+
+            Console.WriteLine("Введіть IP-адресу сайту:");
+            ipAddress = Console.ReadLine();
+        }
+
+        public void DisplayData()
+        {
+            Console.WriteLine($"Назва сайту: {name}");
+            Console.WriteLine($"URL сайту: {url}");
+            Console.WriteLine($"Опис сайту: {description}");
+            Console.WriteLine($"IP-адреса сайту: {ipAddress}");
+        }
     }
 }
