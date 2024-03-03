cc87a03259846e751ae5e21613d41af03b367f34
MaksimMyson <maksimmyson33@gmail.com>
2024-03-03 16:51:26 +0200

    exe6

--- C#3_25_02/Program.cs
+++ C#3_25_02/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace C_3_25_02
+﻿using Core;
+
+namespace C_3_25_02
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;  
+            
+            Plane plane = ReadPlaneInfo();
+
+            
+            plane.DisplayPlaneInfo();
+        }
+
+        public static Plane ReadPlaneInfo()
+        {
+            Console.WriteLine("Введіть назву літака:");
+            string planeName = Console.ReadLine();
+
+            Console.WriteLine("Введіть виробника:");
+            string manufacturer = Console.ReadLine();
+
+            Console.WriteLine("Введіть рік випуску:");
+            int yearOfProduction = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введіть тип літака:");
+            string planeType = Console.ReadLine();
+
+            return new Plane(planeName, manufacturer, yearOfProduction, planeType);
         }
     }
 }
--- Core/Model.cs
+++ Core/Model.cs
@@ -6,7 +6,70 @@ using System.Threading.Tasks;
 
 namespace Core
 {
-    internal class Model
+    public class Plane
     {
+        private string planeName;
+        private string manufacturer;
+        private int yearOfProduction;
+        private string planeType;
+
+        // Конструктор з введенням даних
+        public Plane(string planeName, string manufacturer, int yearOfProduction, string planeType)
+        {
+            this.planeName = planeName;
+            this.manufacturer = manufacturer;
+            this.yearOfProduction = yearOfProduction;
+            this.planeType = planeType;
+        }
+
+        // Метод для виведення даних
+        public void DisplayPlaneInfo()
+        {
+            Console.WriteLine("Назва літака: " + planeName);
+            Console.WriteLine("Виробник: " + manufacturer);
+            Console.WriteLine("Рік випуску: " + yearOfProduction);
+            Console.WriteLine("Тип літака: " + planeType);
+        }
+
+        // Методи доступу до окремих полів класу
+        public void SetPlaneName(string planeName)
+        {
+            this.planeName = planeName;
+        }
+
+        public string GetPlaneName()
+        {
+            return planeName;
+        }
+
+        public void SetManufacturer(string manufacturer)
+        {
+            this.manufacturer = manufacturer;
+        }
+
+        public string GetManufacturer()
+        {
+            return manufacturer;
+        }
+
+        public void SetYearOfProduction(int yearOfProduction)
+        {
+            this.yearOfProduction = yearOfProduction;
+        }
+
+        public int GetYearOfProduction()
+        {
+            return yearOfProduction;
+        }
+
+        public void SetPlaneType(string planeType)
+        {
+            this.planeType = planeType;
+        }
+
+        public string GetPlaneType()
+        {
+            return planeType;
+        }
     }
 }
